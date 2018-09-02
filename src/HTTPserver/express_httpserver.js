c4f813a376f083715350f03060663faa16a22bc4
synaplasticity <synaplasticity@gmail.com>
2018-09-02 12:25:01 +0530

    Happy path test

--- src/HTTPserver/express_httpserver.js
+++ src/HTTPserver/express_httpserver.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const port = 3000
 const app = express()
+var server
 
 app.get('/', (request, response) => {
     console.log(app.request.url)
     response.send('Greetings from ExpressJS')
 })
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Duh... ', err)
-    }
 
-    console.log(`ExpressJS server is listening on ${port}`)
-})
+function startServer(port) {
+    server = app.listen(port, (err) => {
+        if (err) {
+            return console.log('Duh... ', err);
+        }
+        console.log(`ExpressJS server is listening on ${port}`);
+    });
+    return server
+}
+
+function stopServer(server) {
+    server.close()
+}
+
+module.exports = {startServer, stopServer}
+
+// startServer() 
+// stopServer(server)
