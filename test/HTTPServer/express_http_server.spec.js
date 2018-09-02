76254fb14323eeadc156dfd2cbef10894661b0b8
synaplasticity <synaplasticity@gmail.com>
2018-09-02 12:47:25 +0530

    Refactored to extract comon util methods

--- test/HTTPServer/express_http_server.spec.js
+++ test/HTTPServer/express_http_server.spec.js
@@ -2,17 +2,19 @@
 
 const expect = require('chai').expect
 const request = require('request')
-const app = require('../../src/HTTPserver/express_httpserver')
+const util = require('../../src/HTTPserver/express_utils')
+const h_app = require('../../src/HTTPserver/express_httpserver')
 const port = 3000
 var server
+var app = h_app.getApp()
 
 describe('Basic express server tests', () => {
 
     beforeEach(() => {
-        server = app.startServer(port)
+        server = util.startServer(app, port)
     })
 
-    afterEach(() => app.stopServer(server))
+    afterEach(() => util.stopServer(server))
 
     it('Should return a welcome string', (done) => {
         request('http://localhost:3000/', (req, res, body) => {
